319f374ba8fddf56989641f1bcd441174313e72b
Luiz <designarttdev@gmail.com>
2019-09-06 02:28:36 -0300

    Adicionado a parte de Editar Categoria, faltando ainda o cancelar e o deletar.

--- app/src/main/java/com/example/sistemadevendas/EditarCategoria.java
+++ app/src/main/java/com/example/sistemadevendas/EditarCategoria.java
@@ -2,10 +2,15 @@ package com.example.sistemadevendas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class EditarCategoria extends AppCompatActivity {
 
@@ -38,5 +43,34 @@ public class EditarCategoria extends AppCompatActivity {
         edtCategoria.setText(categoria);
         edtDescricao.setText(descricao);
 
+        btnCatEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditarCategoria();
+            }
+        });
+
+    }
+
+    public void EditarCategoria(){
+        try{
+            String id = edtCatId.getText().toString();
+            String categoria = edtCategoria.getText().toString();
+            String descricao = edtDescricao.getText().toString();
+
+            SQLiteDatabase db = openOrCreateDatabase("supervenda", Context.MODE_PRIVATE, null);
+            String sql = "update categoria set categoria = ?,descricao = ? where id=?";
+            SQLiteStatement statement = db.compileStatement(sql);
+            statement.bindString(1, categoria);
+            statement.bindString(2, descricao);
+            statement.bindString(3, id);
+            statement.execute();
+            Toast.makeText(this, "Cadastrado atualizada com Sucesso.", Toast.LENGTH_SHORT).show();
+
+            Intent i = new Intent(getApplicationContext(), Main.class);
+            startActivity(i);
+        } catch (Exception ex){
+            Toast.makeText(this, "Não atualizado!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
